04d1cc31317b60acc4b4cc97d1ba198dd30aefb0
Chris Done <chrisdone@gmail.com>
2019-04-24 16:23:48 +0100

    Add lazy search (silver)

---
+++ src/silver/Filter.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE ViewPatterns #-}
+
+-- | Lazy bytestring.
+
+module Filter where
+
+import           Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Char8 as S8
+import           System.IO
+
+filterHandle :: Handle -> Handle -> String -> IO ()
+filterHandle inh outh (S8.pack -> arg) = do
+  interactHandle inh outh (L8.pack . show . length . filter (S.isInfixOf arg . L.toStrict) . L8.lines)
+
+interactHandle :: Handle -> Handle -> (ByteString -> ByteString) -> IO ()
+interactHandle inh outh f = do
+  s <- L.hGetContents inh
+  L8.hPutStrLn outh (f s)
