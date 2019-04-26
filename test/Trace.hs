778c091498fcdb5bbdddcc1e8be0b443531c9c1c
Chris Done <chrisdone@gmail.com>
2019-04-26 15:43:05 +0100

    Add assertion

--- test/Trace.hs
+++ test/Trace.hs
@@ -1,5 +1,6 @@
 -- |
 
+import           Control.Monad
 import           System.Exit
 import qualified System.Hatrace as Hatrace
 import           Test.Hspec
@@ -9,8 +10,20 @@ main = hspec spec
 
 spec :: SpecWith ()
 spec =
-  describe
-    "filterLine"
-    (it
-       "filterLine = isInfixOf"
-       (Hatrace.traceForkProcess "echo" ["hello"] `shouldReturn` ExitSuccess))
+  before_
+    assertNoChildren
+    (describe
+       "filterLine"
+       (it
+          "filterLine = isInfixOf"
+          (Hatrace.traceForkProcess "echo" ["hello"] `shouldReturn` ExitSuccess)))
+
+-- | Assertion we run before each test to ensure no leftover child processes
+-- that could affect subsequent tests.
+--
+-- This is obviously not effective if tests were to run in parallel.
+assertNoChildren :: IO ()
+assertNoChildren = do
+  hasChildren <- Hatrace.doesProcessHaveChildren
+  when hasChildren $ do
+    error "You have children you don't know of, probably from a previous test"
