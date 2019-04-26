551db27e1ebe103dbc89685389f82654d65b6417
Chris Done <chrisdone@gmail.com>
2019-04-26 12:55:35 +0100

    Add filterLine cpu test

--- test/Instructions.hs
+++ test/Instructions.hs
@@ -1,12 +1,32 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE NumericUnderscores #-}
-import Filter
-import System.CPUInstructionCounter
-import System.IO
+import           Data.String
+import qualified Data.ByteString as S
+import           Data.ByteString (ByteString)
+import           Control.Exception
+import           Filter
+import           System.CPUInstructionCounter
+import           System.IO
 
 data Result = OK | INVALID deriving (Show, Eq)
 
 main :: IO ()
 main = do
+  filterStr
+  fileIO
+
+filterStr :: IO ()
+filterStr = do
+  let !pat = fromString "his"
+      !bs =
+        fromString
+          (replicate 4096 'x' <>
+           "maximum. Kut\195\186zov saw this and merely sighed and shrugged his shoulders.\r")
+  (_, result) <- withInstructionsCounted (evaluate (filterLine pat bs))
+  putStrLn ("filterLine: " ++ "\tCPU instructions: " ++ show result)
+
+fileIO :: IO ()
+fileIO = do
   devNull <- openFile "/dev/null" AppendMode
   putStrLn ""
   results <-
