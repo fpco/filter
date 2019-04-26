af772360082e011b6e13bfd87497ecaf21f4fb7a
Chris Done <chrisdone@gmail.com>
2019-04-26 15:16:26 +0100

    Concentrate specifically on bytes

--- test/Property.hs
+++ test/Property.hs
@@ -1,10 +1,14 @@
 -- |
 
-import Data.List
-import Data.String
-import Filter (filterLine)
-import Test.Hspec
-import Test.QuickCheck
+import           Data.ByteString (ByteString)
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as S8
+import           Data.List
+import           Data.String
+import           Filter (filterLine)
+import           Test.Hspec
+import           Test.QuickCheck
 
 main :: IO ()
 main = hspec spec
@@ -16,4 +20,6 @@ spec =
     (it
        "filterLine = isInfixOf"
        (property
-          (\x xs -> filterLine (fromString x) (fromString xs) == isInfixOf x xs)))
+          (\x xs ->
+             filterLine (fromString x) (fromString xs) ==
+             isInfixOf (S.unpack (S8.pack x)) (S.unpack (S8.pack xs)))))
